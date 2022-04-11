f572d011d05675bf445578713148727dc3813ab0
Matheus Eidt <matheus@mx2.unisc.br>
2022-04-11 19:53:03 -0300

    chore: including docstring and print result of crawler to terminal.

--- strings/idwall_text_wrap.py
+++ strings/idwall_text_wrap.py
@@ -1,4 +1,5 @@
 from textwrap import wrap
+from typing import List
 
 from constants import DEFAULT_INPUT_TEXT, DEFAULT_MAXIMUM_LINE_WIDTH
 from repository import idwall_strings_repository
@@ -6,8 +7,18 @@ from repository import idwall_strings_repository
 
 class IdwallTextWrap:
     def _wrap_text(
-        self, input_text, maximum_line_width, output_filename, use_python_wrap
-    ):
+        self, input_text: str, maximum_line_width: int, use_python_wrap: bool
+    ) -> List[str]:
+        """Wrap an input text.
+
+        Args:
+            input_text (str): The text to be wrapped.
+            maximum_line_width (int): MAximum line length.
+            use_python_wrap (bool): Use python built-in function to wrap.
+
+        Returns:
+            List[str]: The wrapped text in a list.
+        """
         idwall_strings_repository._check_words_size(input_text, maximum_line_width)
         if use_python_wrap:
             wrapped_text = wrap(input_text, maximum_line_width)
@@ -32,10 +43,9 @@ class IdwallTextWrap:
             output_filename (str, optional): The filename in wich text will be saved. Defaults to "output-part1.txt".
             use_python_wrap (bool, optional): Use Python built-in method to wrap. Defaults to False.
         """
-        output_text = self._wrap_text(
-            input_text, maximum_line_width, output_filename, use_python_wrap
-        )
+        output_text = self._wrap_text(input_text, maximum_line_width, use_python_wrap)
         idwall_strings_repository._write_to_file(output_text, output_filename)
+        [print(text) for text in output_text]
 
     def justify_text(
         self,
@@ -52,11 +62,10 @@ class IdwallTextWrap:
             output_filename (str, optional): The output filename to save the results. Defaults to "output-part2.txt".
             use_python_wrap (bool, optional): Use Python built-in method to wrap. Defaults to False.
         """
-        wrapped_text = self._wrap_text(
-            input_text, maximum_line_width, output_filename, use_python_wrap
-        )
+        wrapped_text = self._wrap_text(input_text, maximum_line_width, use_python_wrap)
         splitted_lines = [line.split() for line in wrapped_text]
         output_text = idwall_strings_repository._justify_text_in_list_of_lines(
             maximum_line_width, splitted_lines
         )
         idwall_strings_repository._write_to_file(output_text, output_filename)
+        [print(text) for text in output_text]
